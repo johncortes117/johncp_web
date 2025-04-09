bcf55abab14a20118ca0c9035c3cb2c158046562
John Cortés Pozo <stevencortespozo@gmail.com>
2025-04-09 07:28:10 -0500

    Update Hero.tsx

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -550,7 +550,7 @@ export default function Hero() {
                 transition={{ duration: 0.5, delay: 0.8 }}
               >
                 <Link 
-                  href="https://drive.google.com/file/d/1x54jXSzYkmjPj6RsGao8b-H-i1A0ckd2/view?usp=sharing"
+                  href="https://drive.google.com/file/d/1BhcPQbHDot6wRQU8J7GXRRT6YhZsBV-P/view?usp=sharing"
                   target="_blank"
                   rel="noopener noreferrer"
                 >
