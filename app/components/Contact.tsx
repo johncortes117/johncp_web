02bfa9e4a1ed2b380ac8d6de352c9b9347015219
John Cortés Pozo <stevencortespozo@gmail.com>
2025-07-30 09:20:44 -0500

    Update CV link in Contact

--- app/components/Contact.tsx
+++ app/components/Contact.tsx
@@ -129,7 +129,7 @@ export default function Contact() {
                         software development.
                       </p>
                       <Link 
-                        href="https://drive.google.com/file/d/1BhcPQbHDot6wRQU8J7GXRRT6YhZsBV-P/view?usp=sharing"
+                        href="https://drive.google.com/file/d/1JRb-GWTkZZuWgnfbDtcntzwjAS0iurg_/view?usp=sharing"
                         target="_blank"
                         rel="noopener noreferrer"
                       >
